49c4e9c1f44e70ed690914ac58aa4e68430c706c
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-06-16 15:00:04 +0530

    solved clippy worn

--- src/main.rs
+++ src/main.rs
@@ -46,9 +46,7 @@ fn logic() -> Result<(), Box<dyn std::error::Error>> {
                 file.size()
             );
             if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    fs::create_dir_all(&p)?;
-                }
+                fs::create_dir_all(p)?;
             }
             let mut outfile = fs::File::create(&outpath)?;
             io::copy(&mut file, &mut outfile)?;
