673e1d89ea7541d9d9ad2a13f009c4996a61a541
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-06-16 14:20:39 +0530

    added zip archive taking args only file for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+use std::fs;
+use std::io;
+
+#[derive(Parser)]
+struct Cli {
+    path: std::path::PathBuf, // path of zip file
+}
+
 fn main() {
-    println!("Hello, world!");
+    std::process::exit(logic());
+}
+
+fn logic() -> i32 {
+    let args = Cli::parse();
+    println!("The zip file is : {:?}", args.path);
+    let file = fs::File::open(&args.path).unwrap();
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+    0
 }
